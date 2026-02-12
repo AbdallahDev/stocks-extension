84020fd4472b599de1bdc74cdc85cb2ea26ecc1b
AbdallahDev <abdallah.dev85@gmail.com>
2026-02-12 19:47:17 +0300

    stage

--- script.js
+++ script.js
@@ -22,7 +22,25 @@ stocks.forEach((stock, index) => {
 
   if (index % 2 == 0) stocksLineEl.style.backgroundColor = "#F0F0F0";
 
-  stocksLineEl.innerHTML = `<img src="${stock.logoUrl}" alt="${stock.name}" class="logo">`;
+  //this code makes the stock line html
+  stocksLineEl.innerHTML = `
+  <div class="logoAndNameAndTicker">
+    <img src="${stock.logoUrl}" alt="${stock.name}" class="logo">
+    <div class="nameAndTicker">
+      <div class="name">${stock.name}</div>
+      <div class="ticker">${stock.ticker}</div>
+    </div>
+  </div>
+  <div class="priceAndChange">
+    <div class="priceAndCurrency">
+      <div class="price">${stock.price}</div>
+      <div class="currency">${stock.currency}</div>
+    </div>
+    <div class="changePriceAndPercent">
+      <div class="changePrice">${stock.change_price}</div>
+      <div class="changePercent">${stock.change_percent}</div>
+    </div>
+  </div>`;
 
   stocksTableEl.appendChild(stocksLineEl);
 });
